275dab9cc9e4f18ddb9a5f002fb4baa0c2b80c6d
Kiur Jullis <kiur.jullis@gmail.com>
2022-07-15 09:03:17 +0300

    ..

--- src/pages/Kontakt.js
+++ src/pages/Kontakt.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
-import { Formik } from "formik";
-import * as Yup from 'yup'
+//import { useNavigate } from "react-router-dom";
+//import { Formik } from "formik";
+//import * as Yup from 'yup'
 
 
 
