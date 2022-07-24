43a13b7eab971cca7193765f0188e5a5b6bb55c1
Kiur Jullis <kiur.jullis@gmail.com>
2022-07-24 06:57:43 +0300

    tekst

--- src/pages/Meist.js
+++ src/pages/Meist.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Meist = () => {
   return (
-    <div className="container">Meist</div>
+    <div className="container">
+      <h1>Värsked sõõrikud Võrus</h1>
+      <p>Värsked Sõõrikud OÜ rõõmustab sõõrikusõpru aastast 2017. Valmistame sõõrikuid ettetellimisel. Pakume ehtsaid vanakooli sõõrikuid, valmistatud pärmitaignast ja peal tuhksuhkur. Toome teile sõõrikud kohale kohe peale küpsetamist, värskete ja krõbedatena</p>
+    </div>
   )
 }
 
