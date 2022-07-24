38692cf51cd6838c15d265de7c1f9809fca2abb3
Kiur Jullis <kiur.jullis@gmail.com>
2022-07-24 06:57:35 +0300

    .toLocaleDateString

--- src/pages/TellimusTehtud.js
+++ src/pages/TellimusTehtud.js
@@ -2,13 +2,24 @@ import React from 'react'
 
 const TellimusTehtud = ({ formData }) => {
   console.log(formData)
+
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  var selectedDate = new Date(formData.kuupäev);
+  var formattedDate = selectedDate.toLocaleDateString("et-EE", options);
+
   return (
     <div className="container">
       <h1>Tellimus tehtud</h1>
       <p><span className='capitalize'>{formData.nimi}</span>, aitäh!</p>
       <p>
-        {formData.kogus} kg sõõrikuid ootab teid {formData.kuupäev} kell {formData.aeg}.
-
+        {formattedDate}, kell {formData.aeg}.
+        <br />
+        {formData.kogus} kg sõõrikuid ({parseInt(formData.kogus / 0.08)} tk) ootab teid aadressil Kooli 6, Võru.
+        <br />
+        Summa: {formData.kogus * 8} €
+      </p>
+      <p>
+        Tühista tellimus
       </p>
     </div>
   )
