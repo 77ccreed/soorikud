70d9a6a92c774977750897ce3524cf8cc236ce67
Kiur Jullis <kiur.jullis@gmail.com>
2022-07-26 10:38:47 +0300

    teenusetingimused

--- src/pages/Teenusetingimused.js
+++ src/pages/Teenusetingimused.js
@@ -4,17 +4,7 @@ const Teenusetingimused = () => {
   return (
     <div className="container">
       <h1>Teenusetingimused</h1>
-      <p>
-        Hind. Arve tasumine
-      </p>
-      <p>
-        Miinimum kogus, mida saab tellida.
-      </p>
-      <p>
-        Kättesaamine. Kättesaamise koht. Esimene kättesaamise aeg homsest, kell 12:00.
-      </p>
-      <p>Mille jaoks andmeid kogutakse ja kuidas neid kasutatakse. Esitatud andmed peavad õiged olema.</p>
-      <p>Kuidas käib tellimuse tühistamine.</p>
+
       <h2>1. Tingimuste ulatus ja kehtivus</h2>
       <p>
         1.1.Tingimused kehtivad tellija ja soorikud.ee omaniku Värsked Sõõrikud OÜ (Reg: 14279980) vahel kodulehel pakutava teenuse osutamisel tekkivate vaidluste lahendamisel.
@@ -27,23 +17,33 @@ const Teenusetingimused = () => {
       </p>
       <h2>3. Tellimuse vormistamine</h2>
       <p>
-        3.1. Teenuse tellimiseks vormista tellimusvorm.
+        3.1. Teenuse vormistatakse täites tellimusvormi esitates tõeseid andmeid ja nõustudes teenusetingimustega.
       </p>
       <h2>4. Tellimuse eest tasumine</h2>
       <p>
-
+        4.1. Tellimuse eest tasumine toimub tellimusvormil tellija poolt määratud ajal, aadressil Kooli 6, Võru, 65606
+        Võru maakond.
+        <br />
+        4.2. Tellimuse eest tasumine toimub sularahas.
       </p>
       <h2>5. Tellimuse täitmine</h2>
       <p>
+        5.1. Tellimuse täitmine toimub tellimusvormil tellija poolt määratud ajal, aadressil Kooli 6, Võru, 65606, Võru maakond.
 
       </p>
       <h2>6. Tellimuse tühistamine</h2>
       <p>
-
+        6.1.Tellija saab tellimuse tühistada kodulehe vahendusel. Või helistades telefoni numbrile +372 510 3606.
+        <br />
+        6.2.Tellija saab tühistada ainult tellimusi, mille täitmiseni on rohkem aega kui 12 tundi.
+        <br />
+        6.3.Värsked Sõõrikud OÜ saab tellimuse tühistada helistades tellija poolt määratud telefoninumbril.
       </p>
       <h2>7. Vastutus</h2>
       <p>
-
+        7.1. Värsked Sõõrikud OÜ ja tellija vastutavad teineteise ees käesolevate tingimuste rikkumisega teisele poolele tekitatud kahju eest Eesti Vabariigis kehtivates õigusaktides sätestatud juhtudel ja ulatuses.
+        <br />
+        7.2. Värsked Sõõrikud OÜ ei vastuta tellijale tekitatud kahjude eest või teenuse osutamise viibimise eest juhul, kui kahju või teenuse osutamise viibimine on tingitud asjaolust, mida OÜ Kodulehe Tellimine ei saanud mõjutada, ei näinud ette ega võinud ette näha (vääramatu jõud).
       </p>
       <h2>8. Vaidluste lahendamine</h2>
       <p>
