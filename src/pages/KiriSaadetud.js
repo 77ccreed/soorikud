994b9dbf1d601692a861801b73d8bc94c1105d5d
Kiur Jullis <kiur.jullis@gmail.com>
2022-08-03 07:03:14 +0300

    kiri on saadetud

--- src/pages/KiriSaadetud.js
+++ src/pages/KiriSaadetud.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const KiriSaadetud = () => {
   return (
     <div className="container">
       <h1>Kiri on saadetud.</h1>
-      <p>Vastame esimesel võimalusel.</p>
+      <p>Aitäh. Vastame esimesel võimalusel.</p>
+      <Link to="/">
+        <p>
+          Tagasi avalehele
+        </p>
+      </Link>
     </div>
   )
 }
